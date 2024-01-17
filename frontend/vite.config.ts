b52ebbdde6cf066bf866f027fd1d4824fcc84726
Gauravjot Garaya <gauravjot@outlook.com>
2024-01-16 16:37:31 -0800

    updated gitignore for dist

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    // depending on your application, base can also be "/"
-    base: '',
-    plugins: [react(), viteTsconfigPaths()],
-})
+	// depending on your application, base can also be "/"
+	base: "",
+	plugins: [react(), viteTsconfigPaths()],
+});
